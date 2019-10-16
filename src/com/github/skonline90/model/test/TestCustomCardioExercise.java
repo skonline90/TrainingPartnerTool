8fd9be5d20a2b013f3aaa0fa69ff2d65cf21647b
skonline90 <skonline90@posteo.mx>
2019-10-16 19:31:33 +0200

    Added Junit5 to classpath; created model test package

---
+++ src/com/github/skonline90/model/test/TestCustomCardioExercise.java
@@ -0,0 +1,16 @@
+package com.github.skonline90.model.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCustomCardioExercise
+{
+
+    @Test
+    void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
